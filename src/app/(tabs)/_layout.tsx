d6e8115aa66c1daca50bb3e5b6c6c2c48ead4b26
Le Minh Duy <duy.leminh.se@gmail.com>
2025-06-20 07:35:37 +0700

    Fix UI

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -91,7 +91,6 @@ const TabLayout = () => {
         },
         headerShown: false,
         tabBarLabelStyle: {
-          paddingBottom: 5,
           fontFamily: FONTS.bold,
           fontSize: 12,
         },
@@ -99,8 +98,7 @@ const TabLayout = () => {
         tabBarInactiveTintColor: APP_COLOR.BROWN,
         tabBarStyle: {
           borderTopWidth: 0,
-          height: 80,
-          paddingBottom: 5,
+          height: 55,
           backgroundColor: APP_COLOR.BACKGROUND_ORANGE,
         },
         tabBarLabel:
